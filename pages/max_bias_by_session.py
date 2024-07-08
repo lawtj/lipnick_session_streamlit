8ce6fff2f91e0df81091156a975aec924d54133e
lawtj <41483192+lawtj@users.noreply.github.com>
2024-07-08 16:21:55 -0700

    add color coding

--- pages/max_bias_by_session.py
+++ pages/max_bias_by_session.py
@@ -31,17 +31,28 @@ with st.sidebar:
     
     ## selectbox for session
     max_bias = st.number_input('Show biases greater than', 0, 20, 10, 1)
-    sessionlist = labview_samples[labview_samples['bias'] >= max_bias]['session'].unique().tolist()
+    sessionlist = labview_samples[(labview_samples['bias'] >= max_bias) & (labview_samples['algo']=='keep')]['session'].unique().tolist()
     st.write('Number of sessions: ', len(sessionlist))
     sessionlist.reverse()
     selected_session = st.selectbox('Select a session', sessionlist)
     
     frame = labview_samples[labview_samples['session'] == selected_session]
-    
+    frame = frame.drop(columns=['session'])
+    if frame['Nellcor PM1000N-1/SpO2'].sum() >0:
+        frame = frame.drop(columns=['Masimo 97/SpO2','Nellcor/SpO2', 'Nellcor/SpO2_diff_prev','Nellcor/SpO2_diff_next', 'Masimo 97/SpO2_diff_prev', 'Masimo 97/SpO2_diff_next'])
+        frame = frame.rename(columns={'Nellcor PM1000N-1/SpO2':'Nellcor/SpO2',
+                                      'Nellcor PM1000N-1/SpO2_diff_prev':'Nellcor/SpO2_diff_prev',
+                                      'Nellcor PM1000N-1/SpO2_diff_next':'Nellcor/SpO2_diff_next',
+                                      'Rad97-60/SpO2':"Masimo 97/SpO2",
+                                        'Rad97-60/SpO2_diff_prev':'Masimo 97/SpO2_diff_prev',
+                                        'Rad97-60/SpO2_diff_next':'Masimo 97/SpO2_diff_next'
+                                      })
+    else:
+        frame = frame.drop(columns=['Masimo HB/SpO2','Masimo HB/SpO2_diff_prev','Masimo HB/SpO2_diff_next', 'Nellcor PM1000N-1/SpO2', 'Nellcor PM1000N-1/SpO2_diff_prev', 'Nellcor PM1000N-1/SpO2_diff_next', 'Rad97-60/SpO2', 'Rad97-60/SpO2_diff_prev', 'Rad97-60/SpO2_diff_next'])
     criteria_check_tuple, criteria_check_df = ox.session_criteria_check(frame)
     
 st.markdown('## Session ' + str(selected_session))
-st.write(frame)
+st.write(frame.set_index('sample').drop(columns=['sample_diff_prev', 'sample_diff_next', ]))
 
 plotcolumns = ['so2', 'Nellcor/SpO2','bias']
 from session_functions import colormap
@@ -50,6 +61,16 @@ from session_functions import colormap
 frame['so2_symbol'] = frame['so2_stable'].apply(lambda x: colormap['so2'][1] if x else 'cross')
 frame['Nellcor/SpO2_symbol'] = frame['Nellcor_stable'].apply(lambda x: colormap['Nellcor/SpO2'][1] if x else 'cross')
 frame['bias_symbol'] = colormap['bias'][1]
+
+# create so2_line which = red if so2_stable is False AND bias > bias_threshold, else DarkSlateGrey
+# frame['so2_line'] = np.where((frame['so2_stable'] == True) & (frame['bias'] > max_bias), 'red', 'DarkSlateGrey')
+# ACTUALLY so2_line is always DarkSlateGrey
+frame['so2_line'] = 'DarkSlateGrey'
+
+# same for Nellcor/SpO2
+frame['Nellcor/SpO2_line'] = np.where((frame['Nellcor_stable'] == True) & (frame['bias'] > max_bias), 'red', 'DarkSlateGrey')
+# bias is always DarkSlateGrey
+frame['bias_line'] = 'DarkSlateGrey'
 
 
 fig = go.Figure()
@@ -63,10 +84,12 @@ for column in plotcolumns:
             color= colormap[column][0],
             size=12,
             opacity=0.8,
-            line=dict(width=1, color='DarkSlateGrey')
-        )
+            line=dict(width=1.5, color=frame[column + '_line'])
+        ),
+
     ))
 
+st.write('Crosses indicate that the data point was rejected. Red outlines indicate Nellcor values where the bias is > threshold, but were not cleaned out.')
 st.plotly_chart(fig, use_container_width=True)
 
 st.markdown('#### Session Criteria Check')
